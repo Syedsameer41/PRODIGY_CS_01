b91b3993d22266e4c6b5ac1621605e5421f94c67
syed sameer <syedsameer4186@gmail.com>
2026-09-12 00:29:44 +0530

    Add Caesar Cipher implementation

---
+++ caesar_cipher.js
@@ -0,0 +1,33 @@
+function caesarCipher(message,shift,choice){
+let result = "";
+for (let i = 0; i < message.length; i++){
+let oldLetter = message[i];
+let oldIndex =oldLetter.charCodeAt(0);
+if ( oldIndex >= 65 && oldIndex <= 90 && choice === "encrypt"){
+let minus = oldIndex - 65;
+let newIndex = (shift + minus) % 26;
+let newLetter = String.fromCharCode(newIndex + 65);
+result += newLetter;
+} else if (oldIndex >= 97 && oldIndex <= 122 && choice === "encrypt"){
+let minus = oldIndex - 97;
+let newIndex = (shift + minus) % 26;
+let newLetter = String.fromCharCode(newIndex + 97);
+result += newLetter;
+} else if (oldIndex >= 65 && oldIndex <= 90 && choice === "decrypt"){
+let minus = oldIndex - 65;
+let newIndex = (minus - shift + 26)%26;
+let newLetter =String.fromCharCode(newIndex + 65);
+result += newLetter;
+} else if (oldIndex >= 97 && oldIndex <= 122 && choice === "decrypt"){
+let minus = oldIndex - 97;
+let newIndex = (minus - shift + 26)%26;
+let newLetter =String.fromCharCode(newIndex + 97);
+result += newLetter;
+} else {
+result += oldLetter;
+}
+}
+return result;
+};
+let output =caesarCipher("Zz 123!",1,"encrypt");
+console.log(output);
